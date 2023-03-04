020fbfa12c4ab50fc71573b6ad510123bcd6e50f
smithwithatypo <smithwithatypo@gmail.com>
2023-03-04 00:16:41 -0600

    set up flask backend

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Image.create(
+    prompt="A cute baby sea otter",
+    n=1,
+    size="256x256"
+)
